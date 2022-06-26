5117cbb25aafa3d423dbd7f0f251f2d6bab98a51
DK <kd.000.000.1@gmail.com>
2022-06-25 23:07:49 -0400

    #1 angular v14 upgrade

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ace-interview-exercise'),
+      dir: require('path').join(__dirname, './coverage/alaska-airlines'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ace-interview-exercise'`, () => {
+  it(`should have as title 'alaska-airlines'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ace-interview-exercise');
+    expect(app.title).toEqual('alaska-airlines');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ace-interview-exercise app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('alaska-airlines app is running!');
   });
 });
